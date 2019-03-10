0022624ecfbcac57896dc0f12c88b30971c48e4b
theamit009 <amit.sharma1021669@gmail.com>
2019-03-10 13:57:05 +0530

    'nkcnksjdnc'

--- server.js
+++ server.js
@@ -7,19 +7,20 @@ var app = express();
 app.set('port', process.env.PORT || 5000);
 
 app.use(express.static('public'));
-app.use(bodyParser.json());
+app.use(bodyParser.json());  
+
 
 app.post('/update', function(req, res) {
     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
         // watch for any connect issues
         if (err) console.log(err);
         conn.query(
-            'UPDATE salesforce.Contact SET Phone = $1, MobilePhone = $1 WHERE LOWER(FirstName) = LOWER($2) AND LOWER(LastName) = LOWER($3) AND LOWER(Email) = LOWER($4)',
-            [req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim()],
+            'UPDATE salesforce.Product__c SET Product_Name__c = $1,Price__c = $2 WHERE LOWER(Buyer_Name__c) = LOWER($3) AND LOWER(Buyer_Email__c) = LOWER($4) AND LOWER(Buyer_Phone__c) = LOWER($5)',
+            [req.body.productName.trim(), req.body.productPrice.trim(), req.body.buyerName.trim(), req.body.buyerEmail.trim(), req.body.buyerPhone.trim()],
             function(err, result) {
                 if (err != null || result.rowCount == 0) {
-                  conn.query('INSERT INTO salesforce.Contact (Phone, MobilePhone, FirstName, LastName, Email) VALUES ($1, $2, $3, $4, $5)',
-                  [req.body.phone.trim(), req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim()],
+                  conn.query('INSERT INTO salesforce.Product__c (Product_Name__c, Price__c, Buyer_Name__c, Buyer_Email__c, Buyer_Phone__c) VALUES ($1, $2, $3, $4, $5)',
+                  [req.body.productName.trim(), req.body.productPrice.trim(), req.body.buyerName.trim(), req.body.buyerEmail.trim(), req.body.buyerPhone.trim()],
                   function(err, result) {
                     done();
                     if (err) {
